745f6fee9e23054b1ee73993d73c87c9501a3160
SUMIT <sumitdogra3236@gmail.com>
2021-07-28 23:04:10 +0530

    String functions

---
+++ MODULE_1/JavaScript Intro/StringTraversing.js
@@ -0,0 +1,29 @@
+// .charAt() returns the character at the given index of the string
+// .charCodeAt() method returns the ASCII value of the character
+// parseInt() converts string numbers to integer
+// let str = "Sumit Dogra";
+
+// for(let i = 0; i < str.length; i++){
+//     console.log(str.charCodeAt(i));
+// }
+
+// for(let i = 0; i < str.length; i++){
+//     processI
+// }
+
+// let str = "lkajfebblfb bbb fbbb";    // find the no. of b
+// let count = 0;
+// for(let i = 0; i < str.length; i++){
+//     if(str[i] == 'b'){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+// let number = '12';
+// console.log(number);
+// console.log(parseInt(number)); //parseInt() converts string numbers to integer
+
+
+
+
