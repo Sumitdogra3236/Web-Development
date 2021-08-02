147d1015882986c072255a204de94c08420f658f
SUMIT <sumitdogra3236@gmail.com>
2021-08-02 23:13:22 +0530

    Array

---
+++ MODULE_1/JavaScript Intro/Array.js
@@ -0,0 +1,14 @@
+// new array creation
+// let arr = []; // -> method 1
+// let arr = new Array();
+
+// pop() -> pops the element from the last of the array
+// push() -> adds the element to the last of the array
+// console.log(arr.length);
+
+let arr = ["Sumit", 21, 79, 6.7];
+console.log(arr);
+
+// arr.pop();
+arr.push("Google Canada");
+console.log(arr);
