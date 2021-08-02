97ef25473acba3e645adf2fc0df43c3ad7120641
SUMIT <sumitdogra3236@gmail.com>
2021-08-02 23:31:38 +0530

    Array

--- MODULE_1/JavaScript Intro/Array.js
+++ MODULE_1/JavaScript Intro/Array.js
@@ -7,6 +7,9 @@
 // arr.length -> to get the length of the array, length is a Property
 // .split(" ") -> [converts the string into Array], split the string by space and return an arrya of characters
 // .join("") -> [convert the array to string], joins the array using the given parameter
+// .concat() -> merge two arrays
+
+
 // console.log(arr.length);
 
 // let arr = ["Sumit", 21, 79, 6.7];
@@ -21,8 +24,12 @@
 // console.log(str.split("")); 
 // console.log(str.split(" "));
 
-let arr = ["a", "b", "c", "d"];
-console.log(arr.join(""));
-console.log(arr.join("@"));
-console.log(arr.join("#"));
-console.log(arr.join("$")); 
+// let arr = ["a", "b", "c", "d"];
+// console.log(arr.join(""));
+// console.log(arr.join("@"));
+// console.log(arr.join("#"));
+// console.log(arr.join("$")); 
+
+let arr1 = ["a", "b", "c"];
+let arr2 = ["d", "e", "f"];
+console.log(arr1.concat(arr2));
