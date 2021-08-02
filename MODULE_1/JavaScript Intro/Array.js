c0de9a9f19cb5e9ca564821bebf1655801fed111
SUMIT <sumitdogra3236@gmail.com>
2021-08-02 23:25:36 +0530

    Array

--- MODULE_1/JavaScript Intro/Array.js
+++ MODULE_1/JavaScript Intro/Array.js
@@ -4,11 +4,25 @@
 
 // pop() -> pops the element from the last of the array
 // push() -> adds the element to the last of the array
+// arr.length -> to get the length of the array, length is a Property
+// .split(" ") -> [converts the string into Array], split the string by space and return an arrya of characters
+// .join("") -> [convert the array to string], joins the array using the given parameter
 // console.log(arr.length);
 
-let arr = ["Sumit", 21, 79, 6.7];
-console.log(arr);
+// let arr = ["Sumit", 21, 79, 6.7];
+// console.log(arr);
 
-// arr.pop();
-arr.push("Google Canada");
-console.log(arr);
+// // arr.pop();
+// arr.push("Google Canada");
+// console.log(arr.length);
+
+
+// let str = "Java Script";
+// console.log(str.split("")); 
+// console.log(str.split(" "));
+
+let arr = ["a", "b", "c", "d"];
+console.log(arr.join(""));
+console.log(arr.join("@"));
+console.log(arr.join("#"));
+console.log(arr.join("$")); 
