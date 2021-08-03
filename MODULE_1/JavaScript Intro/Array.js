18a7d87a1a906b197a1bba73029d47ef66e31d51
SUMIT <sumitdogra3236@gmail.com>
2021-08-03 11:40:21 +0530

    Arrays

--- MODULE_1/JavaScript Intro/Array.js
+++ MODULE_1/JavaScript Intro/Array.js
@@ -10,7 +10,7 @@
 // .concat() -> merge two arrays
 // for in loop, for(let i in arr), i represent the indxes
 // for of loop, for(let i of arr), of represent the value of indexes
-
+// String.fromCharCode() -> converts the ascii to character
 // console.log(arr.length);
 
 // let arr = ["Sumit", 21, 79, 6.7];
@@ -36,14 +36,23 @@
 // console.log(arr1.concat(arr2));
 
 
-let arr = [5,6,7,8];
-
+// let arr = [5,6,7,8];
+// traverse and get the indexes
 // for(let i in arr){      // i represent the indexes
+    // console.log(parseInt(i));
+    // console.log(arr[i]);
+// }
+
+//traverse and get the value
+// for(let i of arr){          // i represent the value of arr
 //     console.log(i);
-//     console.log(arr[i]);
 // }
 
 
-for(let i of arr){          // i represent the value of arr
-    console.log(i);
-}
+// console.log("a" + 1 + 2);   // string concatenation 
+// console.log("a" + (1 + 3));
+// console.log(1 + 2 + "a");   // first integer addition then string concatenation
+
+// for convert the character from the ascii
+console.log(String.fromCharCode("A".charCodeAt() + 1));
+
