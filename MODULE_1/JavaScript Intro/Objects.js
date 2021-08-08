27c53d5c56d66ec49a202c72292434c56a797e4b
SUMIT <sumitdogra3236@gmail.com>
2021-08-08 21:00:20 +0530

    Objects

---
+++ MODULE_1/JavaScript Intro/Objects.js
@@ -0,0 +1,10 @@
+// To declare a object
+// let obj = {};
+
+let person = {
+    "Name" : "Sumit Dogra",
+    Age : "21",
+    Phone : "1234567890" ,
+    0 : "Hello"
+}
+console.log(person[0]) // when there is numerical key, person.0 fails, must use []
