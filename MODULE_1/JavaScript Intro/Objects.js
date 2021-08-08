7060facc43498588ac2857514c188c50041af558
SUMIT <sumitdogra3236@gmail.com>
2021-08-08 22:16:31 +0530

    Object

--- MODULE_1/JavaScript Intro/Objects.js
+++ MODULE_1/JavaScript Intro/Objects.js
@@ -3,21 +3,30 @@
 // To declare a object
 // let obj = {};
 // in object, key is also known as property
-let person = {
-    "Name" : "Sumit Dogra",
-    Age : "21",
-    Phone : "1234567890" ,
-    0 : "Hello"
-}
-person["Company"] = "Facebook"; // way to add a key to a object
-person["Git-Hub"] = "https://github.com/Sumitdogra3236"; // way to add a key to a object
-person.gender = "Male";
+// let person = {
+//     "Name" : "Sumit Dogra",
+//     Age : "21",
+//     Phone : "1234567890" ,
+//     0 : "Hello"
+// }
+// person["Company"] = "Facebook"; // way to add a key to a object
+// person["Git-Hub"] = "https://github.com/Sumitdogra3236"; // way to add a key to a object
+// person.gender = "Male";
 // console.log(person[0]) // when there is numerical key, person.0 fails, must use []
 // arr.1 -> dot property cannot be used on the number key
 // console.log(person)
 
 // Prototype
 // console.log(Array.prototype);
-Array.prototype.name = "Sumit"; // name is the prototype of the array
-let arr = [1,2,3,4];
-console.log(arr.name);
+// Array.prototype.name = "Sumit"; // name is the prototype of the array
+// let arr = [1,2,3,4];
+// console.log(arr.name);
+
+String.prototype.len = function() {
+    // console.log(this.length);
+    return(this.length);
+}
+
+let str = "Sumit Dogra";
+// str.len();
+console.log(str.len())
