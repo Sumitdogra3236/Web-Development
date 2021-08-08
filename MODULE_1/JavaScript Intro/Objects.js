198dabccbd3712c0afb49eb43e5c9c406871f366
SUMIT <sumitdogra3236@gmail.com>
2021-08-08 21:57:50 +0530

    Object

--- MODULE_1/JavaScript Intro/Objects.js
+++ MODULE_1/JavaScript Intro/Objects.js
@@ -1,6 +1,8 @@
+// Methods -> any function defined in an object is known as Method of that object.
+
 // To declare a object
 // let obj = {};
-
+// in object, key is also known as property
 let person = {
     "Name" : "Sumit Dogra",
     Age : "21",
@@ -11,4 +13,5 @@ person["Company"] = "Facebook"; // way to add a key to a object
 person["Git-Hub"] = "https://github.com/Sumitdogra3236"; // way to add a key to a object
 person.gender = "Male";
 // console.log(person[0]) // when there is numerical key, person.0 fails, must use []
+// arr.1 -> dot property cannot be used on the number key
 console.log(person)
