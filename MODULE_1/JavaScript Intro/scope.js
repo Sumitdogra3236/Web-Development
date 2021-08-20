247139b81921432619478a41d3c3d9e80c770a0e
SUMIT <sumitdogra3236@gmail.com>
2021-08-20 08:59:19 +0530

    Scope

---
+++ MODULE_1/JavaScript Intro/scope.js
@@ -0,0 +1,24 @@
+// var has a functional / functioned scope
+// let and var has block scope
+// let is more secure then var
+
+// var is limited to functional scope and let is limited to block scope.
+// No one knows var outside function scope, and no one knows let outside block scope.
+
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+// console.log(a);     // give error coz, a scope is limited to curly braces / block.
+
+
+
+// {
+//   var a = 10;
+// }
+// console.log(a); // will print a coz var has functional / functioned scope
+
+function abc() {
+  var a = 10;
+}
+console.log(a); // give error coz, var has a functional /functioned scope
