d79f3fed6a24d5bf765e46ede482313dc9a79e57
SUMIT <sumitdogra3236@gmail.com>
2021-08-20 09:00:17 +0530

    Scope

--- MODULE_1/JavaScript Intro/scope.js
+++ MODULE_1/JavaScript Intro/scope.js
@@ -1,6 +1,6 @@
 // var has a functional / functioned scope
 // let and var has block scope
-// let is more secure then var
+// let is more secure then var, coz curly braces is smaller scope than functional scope.
 
 // var is limited to functional scope and let is limited to block scope.
 // No one knows var outside function scope, and no one knows let outside block scope.
