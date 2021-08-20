c8b71d7b1a8e6d94d0e2a98370b9879bb154807b
SUMIT <sumitdogra3236@gmail.com>
2021-08-20 08:51:22 +0530

    Scope

--- MODULE_1/JavaScript Intro/scope.js
+++ MODULE_1/JavaScript Intro/scope.js
@@ -1,4 +1,15 @@
+// var has a function scope
+
+
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+// console.log(a);     // give error coz, a scope is limited to curly braces.
+
+
+
 {
-    let a = 10;
+  var a = 10;
 }
-console.log(a);     // give error coz, a scope is limited to curly braces.
+console.log(a); // will print a coz var has function scope
