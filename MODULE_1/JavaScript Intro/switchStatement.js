3852feb2479180f8f5b2cfe4f555e031dd7f9391
SUMIT <sumitdogra3236@gmail.com>
2021-09-01 12:35:12 +0530

    Switch Statement

---
+++ MODULE_1/JavaScript Intro/switchStatement.js
@@ -0,0 +1,31 @@
+let day = "fri";
+
+switch (day) {
+  case "mon": {
+    console.log("This is Monday");
+    break;
+  }
+  case "tue": {
+    console.log("This is Tuesday");
+    break;
+  }
+  case "wed": {
+    console.log("This is Wednesday");
+    break;
+  }
+  case "thu": {
+    console.log("This is Thursday");
+    break;
+  }
+  case "fri": {
+    console.log("This is Friday");
+    break;
+  }
+  case "sat": {
+    console.log("This is Saturday");
+    break;
+  }
+  default: {
+    console.log("This is Fun Day : Sunday");
+  }
+}
