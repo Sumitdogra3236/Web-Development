8bd4ef4ce4be4f3a319313d742f4047e64675b65
SUMIT <sumitdogra3236@gmail.com>
2021-09-13 20:35:23 +0530

    JS

--- MODULE_1/Web_Scrapping/CricInfo/PlayerOfTheMatch.js
+++ MODULE_1/Web_Scrapping/CricInfo/PlayerOfTheMatch.js
@@ -6,6 +6,16 @@
 // > -> direct child
 */
 
+/*
+request("https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/full-scorecard", requestCallback);
+
+function requestCallback (err, res, html) {
+    const $ = cheerio.load(html);
+    console.log($(".playerofthematch-name").length);
+    console.log($($(".playerofthematch-name")[1]).get(0).attribs);
+}
+*/
+
 let matchlink = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/live-cricket-score";
 const request = require("request");
 const cheerio = require("cheerio");
